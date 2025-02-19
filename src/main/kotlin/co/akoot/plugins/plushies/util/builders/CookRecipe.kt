dac98d65153ee16e1bcdc34a903d003d12e457e8
IndiaPaleAle <154647446+Maltsburg@users.noreply.github.com>
2025-02-19 14:23:35 -0800

    better cook recipes - flugin now using the plushie cook recipe builder lol - made builder better

--- src/main/kotlin/co/akoot/plugins/plushies/util/builders/CookRecipe.kt
+++ src/main/kotlin/co/akoot/plugins/plushies/util/builders/CookRecipe.kt
@@ -1,11 +1,12 @@
 package co.akoot.plugins.plushies.util.builders
 
+import co.akoot.plugins.bluefox.util.TimeUtil.parseTime
 import org.bukkit.Bukkit
 import org.bukkit.NamespacedKey
 import org.bukkit.inventory.*
 
 class CookRecipe private constructor(
-    val name: String, private val input: RecipeChoice, private val result: ItemStack, private val cookTime: Int = 200, private val xp: Float = 0.7f) {
+    val name: String, private val input: RecipeChoice, private val result: ItemStack, private val cookTime: String, private val xp: Double) {
     private val recipes = mutableListOf<Recipe>()
 
     // furnace only
@@ -15,7 +16,7 @@ class CookRecipe private constructor(
 
         Bukkit.removeRecipe(recipe)
 
-        val smeltRecipe = FurnaceRecipe(recipe, result, input, xp, cookTime)
+        val smeltRecipe = FurnaceRecipe(recipe, result, input, xp.toFloat(), parseTime(cookTime, true).toInt())
 
         // Register the recipe
         Bukkit.addRecipe(smeltRecipe)
@@ -31,7 +32,7 @@ class CookRecipe private constructor(
 
         val blastRecipe = BlastingRecipe(
             recipe,
-            result, input, xp, cookTime / 2 // might as well be the same as vanilla
+            result, input, xp.toFloat(), parseTime(cookTime, true).toInt() / 2 // might as well be the same as vanilla
         )
 
         // Register the recipe
@@ -48,11 +49,11 @@ class CookRecipe private constructor(
 
 
         val smokerRecipe = SmokingRecipe(sRecipe,
-            result, input, xp, cookTime / 2 // also the same as vanilla
+            result, input, xp.toFloat(), parseTime(cookTime, true).toInt() / 2 // also the same as vanilla
         )
 
         val campfireRecipe = CampfireRecipe(cfRecipe,
-            result, input, xp, cookTime * 3 // vanilla is 600 ticks, so lets just triple it
+            result, input, xp.toFloat(), parseTime(cookTime, true).toInt() * 3 // vanilla is 600 ticks, so lets just triple it
         )
 
         // Register the recipes
@@ -69,8 +70,8 @@ class CookRecipe private constructor(
     }
 
     companion object {
-        fun builder(name: String, input: RecipeChoice, result: ItemStack, cookTime: Int = 200, xp: Float = 0.7f): CookRecipe {
-            return CookRecipe(name, input, result, cookTime, xp)
+        fun builder(name: String, input: RecipeChoice, result: ItemStack, cookTime: String? = null, xp: Double? = null): CookRecipe {
+            return CookRecipe(name, input, result, cookTime?: "10s", xp?: 0.7)
         }
     }
 }
