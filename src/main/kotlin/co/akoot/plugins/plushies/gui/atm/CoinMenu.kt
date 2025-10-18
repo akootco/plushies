0674147577f7d07a7a704f14fc3e745155f1258c
IndiaPaleAle <154647446+Maltsburg@users.noreply.github.com>
2025-10-18 01:24:27 -0700

    CoinMenu finally done

--- src/main/kotlin/co/akoot/plugins/plushies/gui/atm/CoinMenu.kt
+++ src/main/kotlin/co/akoot/plugins/plushies/gui/atm/CoinMenu.kt
@@ -1,17 +1,61 @@
 package co.akoot.plugins.plushies.gui.atm
 
 import co.akoot.plugins.bluefox.api.economy.Coin
+import co.akoot.plugins.bluefox.api.economy.Economy
+import co.akoot.plugins.bluefox.api.economy.Wallet
+import co.akoot.plugins.bluefox.api.economy.Wallet.Companion.WORLD
 import co.akoot.plugins.bluefox.extensions.wallet
 import co.akoot.plugins.bluefox.util.ColorUtil.randomColor
 import co.akoot.plugins.bluefox.util.Text
+import co.akoot.plugins.bluefox.util.runLater
 import co.akoot.plugins.plushies.util.builders.ChestGUI
 import org.bukkit.entity.Player
+import org.bukkit.event.inventory.InventoryClickEvent
+import org.bukkit.event.inventory.InventoryCloseEvent
 import org.bukkit.inventory.Inventory
 import org.bukkit.inventory.InventoryHolder
 import org.bukkit.inventory.ItemStack
 import java.math.BigDecimal
 
-class CoinMenu(private val p: Player, private val coin: Coin) : InventoryHolder {
+class CoinMenu(private val p: Player, val coin: Coin) : InventoryHolder {
+
+    companion object {
+        fun onClick(holder: CoinMenu, event: InventoryClickEvent) {
+            val allowedTypes = listOfNotNull(holder.coin.backing, holder.coin.backingBlock)
+
+            if ((!event.cursor.isEmpty && event.cursor.type !in allowedTypes) ||
+                (event.currentItem?.type !in allowedTypes)) {
+                event.isCancelled = true
+            }
+        }
+
+        fun onClose(holder: CoinMenu, player: Player, event: InventoryCloseEvent) {
+            runLater(1) {
+                var total = 0
+
+                event.inventory.contents.filterNotNull().forEach { item ->
+                    when (item.type) {
+                        holder.coin.backing -> total += item.amount
+                        holder.coin.backingBlock -> total += item.amount * 9
+                        else -> player.dropItem(item)
+                    }
+                }
+
+                val wallet = player.wallet ?: return@runLater
+                val balance = wallet.balance[holder.coin]?.toInt() ?: 0
+
+                if (total == balance) return@runLater
+
+                if (total > balance) {
+                    WORLD.send(wallet, holder.coin, BigDecimal(total - balance))
+                } else {
+                    wallet.send(Wallet.WORLD, holder.coin, BigDecimal(balance - total))
+                }
+
+                Economy.sendBalance(player, wallet, holder.coin)
+            }
+        }
+    }
 
     override fun getInventory(): Inventory {
         val inv = ChestGUI.builder(54, this, false)
--- src/main/kotlin/co/akoot/plugins/plushies/listeners/GUI.kt
+++ src/main/kotlin/co/akoot/plugins/plushies/listeners/GUI.kt
@@ -1,6 +1,5 @@
 package co.akoot.plugins.plushies.listeners
 
-import co.akoot.plugins.bluefox.util.runLater
 import co.akoot.plugins.plushies.gui.BookArchiveMenu
 import co.akoot.plugins.plushies.gui.BookArchiveMenu.Companion.bookArchiveMenu
 import co.akoot.plugins.plushies.gui.BookMenu
@@ -12,14 +11,11 @@ import co.akoot.plugins.plushies.gui.PlushieMenu.Companion.plushMenu
 import co.akoot.plugins.plushies.gui.atm.ATMMenu
 import co.akoot.plugins.plushies.gui.atm.ATMMenu.Companion.atmMainMenu
 import co.akoot.plugins.plushies.gui.atm.CoinMenu
-import org.bukkit.Material
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
-import org.bukkit.event.inventory.InventoryAction
 import org.bukkit.event.inventory.InventoryClickEvent
 import org.bukkit.event.inventory.InventoryCloseEvent
-import org.bukkit.event.inventory.InventoryDragEvent
 
 class GUI : Listener {
     @EventHandler
@@ -57,30 +53,8 @@ class GUI : Listener {
                 event.isCancelled = true
             }
 
-            is CoinMenu -> {
-                event.isCancelled = true
-                // this doesn't work yet!
-//                val allowed = listOf(holder.coin.backing, holder.coin.backingBlock)
-//
-//                if (event.cursor.type !in allowed || event.currentItem?.type !in allowed) {
-//                    event.isCancelled = true
-//                }
-            }
+            is CoinMenu -> CoinMenu.onClick(holder, event)
 
-            else -> return
-        }
-    }
-
-    @EventHandler
-    fun onInventoryDrag(event: InventoryDragEvent) {
-        when (val holder = event.inventory.holder) {
-            is CoinMenu -> {
-                event.isCancelled = true
-//                for ((_, item) in event.newItems) {
-//                    if (item.type !in listOf(holder.coin.backing, holder.coin.backingBlock))
-//                        event.isCancelled = true
-//                }
-            }
             else -> return
         }
     }
@@ -88,11 +62,8 @@ class GUI : Listener {
     @EventHandler
     fun onInvClose(event: InventoryCloseEvent) {
         val p = event.player as Player
-        when (event.inventory.holder) {
-            is CoinMenu -> {
-                // 1 tick delay or the server crashes, pretty cool
-                runLater(1) { p.openInventory(ATMMenu(p).inventory) }
-            }
+        when (val holder = event.inventory.holder) {
+            is CoinMenu -> CoinMenu.onClose(holder, p, event)
         }
     }
 }
