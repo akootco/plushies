fdd6e5f31c6efee5d3f0cae6e58491cddae37425
IndiaPaleAle <154647446+Maltsburg@users.noreply.github.com>
2026-09-26 02:26:53 -0700

    teleport custom block instead of replacing it when pushed

--- src/main/kotlin/co/akoot/plugins/plushies/util/CustomBlocks.kt
+++ src/main/kotlin/co/akoot/plugins/plushies/util/CustomBlocks.kt
@@ -5,29 +5,16 @@ import co.akoot.plugins.bluefox.extensions.hasPDC
 import co.akoot.plugins.bluefox.extensions.removePDC
 import co.akoot.plugins.bluefox.extensions.setPDC
 import co.akoot.plugins.bluefox.util.runLater
-import co.akoot.plugins.bluefox.util.text
 import co.akoot.plugins.plushies.Plushies.Companion.key
 import co.akoot.plugins.plushies.events.RemoveCustomBlockEvent
 import co.akoot.plugins.plushies.util.Items.getItem
 import co.akoot.plugins.plushies.util.Util.getBlockPDC
-import co.akoot.plugins.plushies.util.builders.ItemBuilder
-import me.arcaniax.hdb.api.HeadDatabaseAPI
-import net.kyori.adventure.text.Component
 import org.bukkit.Location
-import org.bukkit.Material
 import org.bukkit.block.Block
 import org.bukkit.block.BlockFace
 import org.bukkit.block.Container
-import org.bukkit.block.data.Directional
-import org.bukkit.entity.Display.Brightness
-import org.bukkit.entity.EntityType
 import org.bukkit.entity.ItemDisplay
-import org.bukkit.entity.TextDisplay
-import org.bukkit.inventory.ItemStack
 import org.bukkit.util.BoundingBox
-import org.bukkit.util.Transformation
-import org.joml.AxisAngle4f
-import org.joml.Vector3f
 
 val blockKey = key("block_data")
 val texturedkKey = key("block_textured")
@@ -76,8 +63,10 @@ fun handlePiston(location: Location, direction: BlockFace) {
         location.clone()
             .add(direction.modX.toDouble(), direction.modY.toDouble(), direction.modZ.toDouble())
 
-    removeCustomBlock(location)
-    createDisplay(newLocation, value.split("|").getOrNull(1) ?: "")
+
+    for (entity in location.world.getNearbyEntities(BoundingBox.of(location.block))) {
+        if (entity is ItemDisplay) entity.teleport(newLocation.toCenterLocation())
+    }
 
     runLater(1) { newLocation.chunk.setPDC(getBlockPDC(newLocation), value) }
 }
